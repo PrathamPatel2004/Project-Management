0b33a0f22dff9a0322dad20344a23b30f316ab5a
PrathamPatel2004 <137031619+PrathamPatel2004@users.noreply.github.com>
2026-02-14 04:18:17 +0530

    Backend setup with database connect, dependencies and scripts

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./Config/connnectDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send({ message: "Backend is running on port " + PORT });
+})
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    })
+});
